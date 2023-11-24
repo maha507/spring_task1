680c35a8f82abbcb40d29af0835eaa1f359474a5
maha507 <132257569+maha507@users.noreply.github.com>
2023-11-24 12:54:14 +0530

    initial commit

---
+++ src/main/java/spring/task1/Helloworld.java
@@ -0,0 +1,22 @@
+package spring.task1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class Helloworld extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=req.getParameter("name");
+		PrintWriter printWriter=res.getWriter();
+		printWriter.print("<h1>"+"hello "+ name +" </h1>");
+	}
+
+}
